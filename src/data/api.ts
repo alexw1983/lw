cf2f0cbc7b1c131f3dcd69cdf941a28b9ca65d20
Alex Wilson <alex.wilson@redington.co.uk>
2020-05-29 22:17:07 +0100

    refactored API

--- src/data/api.ts
+++ src/data/api.ts
@@ -4,43 +4,15 @@ import { IPlayer, IAdventure } from "../redux/types";
  * Loads players from the API
  */
 export const getPlayers = async () => {
-  const current = JSON.parse(localStorage.getItem("players"));
+  const current = _load<IPlayer[]>("players");
 
   return {
     json: current ?? [],
   };
 };
 
-export const addPlayer = async (player: IPlayer) => {
-  const current = JSON.parse(localStorage.getItem("players"));
-  if (!current) {
-    localStorage.setItem("players", JSON.stringify([player]));
-  } else {
-    localStorage.setItem("players", JSON.stringify([...current, player]));
-  }
-
-  return true;
-};
-
 export const upsertPlayer = async (player: IPlayer) => {
-  const current = _load<IPlayer[]>("players");
-  if (!current) {
-    _save("players", [player]);
-  } else {
-    if (current.find((x) => x.id === player.id)) {
-      const newPlayers = current.reduce((acc, curr) => {
-        if (curr.id !== player.id) {
-          return [...acc, curr];
-        } else {
-          return [...acc, player];
-        }
-      }, []);
-
-      _save("players", newPlayers);
-    } else {
-      _save("players", [...current, player]);
-    }
-  }
+  _upsert<IPlayer>("players", player, (a, b) => a.id === b.id);
 };
 
 export const getAdventures = async () => {
@@ -70,37 +42,64 @@ export const deleteAdventure = async (adventure: IAdventure) => {
 };
 
 export const upsertAdventure = async (adventure: IAdventure) => {
-  const current = _load<IAdventure[]>("adventures");
+  _upsert<IAdventure>(
+    "adventures",
+    adventure,
+    (a, b) => a.bookNumber === b.bookNumber && a.playerId === b.playerId
+  );
+
+  // const current = _load<IAdventure[]>("adventures");
+  // if (!current) {
+  //   _save("adventures", [adventure]);
+  // } else {
+  //   if (
+  //     current.find(
+  //       (x) =>
+  //         x.bookNumber === adventure.bookNumber &&
+  //         x.playerId === adventure.playerId
+  //     )
+  //   ) {
+  //     const newAdventures = current.reduce((acc, curr) => {
+  //       if (
+  //         curr.bookNumber !== adventure.bookNumber ||
+  //         curr.playerId !== adventure.playerId
+  //       ) {
+  //         return [...acc, curr];
+  //       } else {
+  //         return [...acc, adventure];
+  //       }
+  //     }, []);
+
+  //     _save("adventures", newAdventures);
+  //   } else {
+  //     _save("adventures", [...current, adventure]);
+  //   }
+  // }
+};
+
+const _upsert = <T>(key: string, item: T, matchFn: (a: T, b: T) => boolean) => {
+  const current = _load<T[]>(key);
   if (!current) {
-    _save("adventures", [adventure]);
+    _save(key, [item]);
   } else {
-    if (
-      current.find(
-        (x) =>
-          x.bookNumber === adventure.bookNumber &&
-          x.playerId === adventure.playerId
-      )
-    ) {
-      const newAdventures = current.reduce((acc, curr) => {
-        if (
-          curr.bookNumber !== adventure.bookNumber ||
-          curr.playerId !== adventure.playerId
-        ) {
-          return [...acc, curr];
+    if (current.find((x) => matchFn(x, item))) {
+      const newItems = current.reduce((acc, curr) => {
+        if (matchFn(curr, item)) {
+          return [...acc, item];
         } else {
-          return [...acc, adventure];
+          return [...acc, curr];
         }
       }, []);
 
-      _save("adventures", newAdventures);
+      _save(key, newItems);
     } else {
-      _save("adventures", [...current, adventure]);
+      _save(key, [...current, item]);
     }
   }
 };
 
 const _load = <T>(key: string) => {
-  return JSON.parse(localStorage.getItem("adventures")) as T;
+  return JSON.parse(localStorage.getItem(key)) as T;
 };
 
 const _save = (key: string, item: any) => {
